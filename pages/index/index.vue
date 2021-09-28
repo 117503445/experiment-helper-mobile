2cfd064aa195619934cf69480c68e3a781c2dda9
117503445 <t117503445@gmail.com>
2021-09-28 23:23:25 +0800

    feat: init

---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello World'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+</style>

753453503ef87398e07a7e724cb4778abc6f0228
117503445 <t117503445@gmail.com>
2021-09-29 00:27:37 +0800

    feat: lab-item

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,37 +1,52 @@
 <template>
-	<view class="content">
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="content">
+        <lab-item v-for="item in items" :properties="item.properties" :key="item.id"></lab-item>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello World'
-			}
-		},
-		onLoad() {
+    export default {
+        data() {
+            let uiItems = [{
+                "type": "textbox",
+                "properties": {
+                    "frontText": "hello",
+                    "backText": "-hello"
+                }
+            }, {
+                "type": "textbox",
+                "properties": {
+                    "frontText": "world",
+                    "backText": "-hello"
+                }
+            }]
+            for (let i = 0; i < uiItems.length; i++) {
+                uiItems[i]["id"] = i;
+            }
+            return {
+                title: 'Hello World',
+                items: uiItems
+            }
+        },
+        onLoad() {
 
-		},
-		methods: {
+        },
+        methods: {
 
-		}
-	}
+        }
+    }
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+    .text-area {
+        display: flex;
+        justify-content: center;
+    }
 </style>

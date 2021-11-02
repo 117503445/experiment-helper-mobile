3c69e34150312d3f9f8cf49ba10babbc11888f2b
lxy-littlechai <76553203+lxy-littlechai@users.noreply.github.com>
2021-11-02 20:00:28 +0800

    style：feedback-index

--- src/pages/feedback-index/feedback-index.vue
+++ src/pages/feedback-index/feedback-index.vue
@@ -3,23 +3,24 @@
     <view class="feedback-text">
       <view class="feedback-title">建议反馈</view>
       <view class="feedback-content">
-        <p>"XDU物理实验小助手"是由西电为之工作室开发</p>
-        <p>的项目，旨在为西电学子提供更便捷的服务</p>
-        <p>为之也会持续更新完善，如果你在使用时</p>
-        <p>遇到任何问题，欢迎联系我们</p>
-        <p>联系邮箱:</p>
-        <p>{{ msg.email }}</p>
-        <p>联系微信:</p>
-        <p>{{ msg.wechat }}</p>
-        <p>或者添加我们的QQ群和大家一起让小助手变得更好</p>
+        <text class="feedback-description" decode="true">
+          {{ text }}
+        </text>
       </view>
     </view>
     <image class="feedback-image" :src="msg.image" mode="aspectFit" @click="previewImage(msg.image)"></image>
+    <p class="qq-group">QQ群:{{ msg.qq }}</p>
     <view class="feedback-tips">
-      <p>QQ群:{{ msg.qq }}</p>
-      <p>长按图片识别二维码</p>
-      <a href="https://qm.qq.com/cgi-bin/qm/qr?k=WKBEf4In2yBIwYLB9eIMnQYm_lTdnyfC&jump_from=webapi">点击跳转</a>
-      <p>进入群聊</p>
+      <view>
+        <p class="tap">也可以</p>
+        <a class="tap" href="https://qm.qq.com/cgi-bin/qm/qr?k=WKBEf4In2yBIwYLB9eIMnQYm_lTdnyfC&jump_from=webapi"
+          >点击跳转至QQ</a
+        >
+        <p class="tap">直接进入群聊</p>
+      </view>
+      <view>
+        <p class="tap">(好耶，一步到位新方法，抱紧研发大腿)</p>
+      </view>
     </view>
   </view>
 </template>
@@ -30,7 +31,8 @@ import { UserClient } from "@t117503445/s3-database/user/UserClient";
 export default {
   data() {
     return {
-      msg: ""
+      msg: "",
+      text: '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"XDU物理实验小助手"是由西电为之工作室开发的项目，旨在为西电学子提供更便捷的服务，为之也会持续更新完善，如果你在使用时 遇到任何问题，欢迎联系我们。可以扫描二维码添加我们的QQ群和大家一起让小助手变得更好'
     };
   },
   methods: {
@@ -49,28 +51,44 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .feedback-index-container {
   text-align: center;
   margin-top: 100rpx;
 }
 .feedback-title {
-  font-size: 35rpx;
+  font-size: 40rpx;
   margin: 40rpx 0 20rpx 0;
 }
-.feedback-content p {
-  font-size: 28rpx;
-  margin-bottom: 10rpx;
+.feedback-content text {
+  display: inline-block;
+  font-size: 32rpx;
+  font-weight: 530;
+  margin: 20rpx 60rpx 20rpx 60rpx;
+  text-align: left;
 }
 .feedback-image {
-  height: 250rpx;
-  width: 250rpx;
+  height: 290rpx;
+  width: 290rpx;
 }
-.feedback-tips p,
-a {
+.qq-group {
   display: block;
-  font-size: 23rpx;
+  font-size: 33rpx;
   color: #cccccc;
   margin-bottom: 10rpx;
+}
+.feedback-tips {
+  margin-top: 30rpx;
+}
+.feedback-tips .tap {
+  display: inline-block;
+  font-size: 28rpx;
+  color: black;
+  margin-bottom: 10rpx;
+}
+.feedback-tips a {
+  font-size: 35rpx !important;
+  color: dodgerblue !important;
+  margin: 0 10rpx 0 10rpx;
 }
 </style>

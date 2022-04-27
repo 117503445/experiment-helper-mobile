be5432c05fb400afe0fb1f29354f36c58f0a1385
skyclouds2001 <skyclouds2001@163.com>
2022-04-27 21:28:03 +0800

    在实验详情页加入评论区

--- src/pages/lab-index/lab-index.vue
+++ src/pages/lab-index/lab-index.vue
@@ -9,6 +9,7 @@
       <button class="btn-compute btn-style" @click="calculate">计算结果</button>
     </view>
     <p class="lab-tips">· 数据来自微信公众号-对策府库</p>
+    <Lab-comment :name="experimentName"></Lab-comment>
     <Lab-feedback></Lab-feedback>
     <view class="space"></view>
     <view class="author">——为之工作室——</view>
@@ -16,159 +17,174 @@
 </template>
 
 <script>
-import { Binder, util, experiments } from "@t117503445/experiment-helper-core";
-import LabFeedback from "../../components/lab-item/lab-feedback.vue";
-import LabItem from "../../components/lab-item/lab-item.vue";
+  import {
+    Binder,
+    util,
+    experiments
+  } from "@t117503445/experiment-helper-core";
+  import LabFeedback from "../../components/lab-item/lab-feedback.vue";
+  import LabItem from "../../components/lab-item/lab-item.vue";
+  import LabComment from "../../components/lab-comment/comment-content.vue";
 
-export default {
-  components: {
-    LabFeedback,
-    LabItem
-  },
-  data() {
-    /* util.p(experiments); */
-    return {
-      items: "",
-      experiment: "",
-      binder: "",
-      experiments: experiments,
-      experimentName: "",
-      isWatch: false
-    };
-  },
-  methods: {
-    calculate() {
-      console.log("before calculate items", this.items);
-      this.items = this.binder.calculateLabItems(this.items);
-      console.log("after calculate items", this.items);
+  export default {
+    components: {
+      LabFeedback,
+      LabItem,
+      LabComment
     },
-    reset() {
-      this.items = this.binder.getLabItems(false);
-      /* console.log(this.items); */
+    data() {
+      /* util.p(experiments); */
+      return {
+        items: "",
+        experiment: "",
+        binder: "",
+        experiments: experiments,
+        experimentName: "",
+        isWatch: false
+      };
     },
-    dataStorage() {
-      console.log("保存");
+    methods: {
+      calculate() {
+        console.log("before calculate items", this.items);
+        this.items = this.binder.calculateLabItems(this.items);
+        console.log("after calculate items", this.items);
+      },
+      reset() {
+        this.items = this.binder.getLabItems(false);
+        /* console.log(this.items); */
+      },
+      dataStorage() {
+        console.log("保存");
+        try {
+          uni.setStorageSync(this.experimentName, this.items);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    },
+    onLoad(option) {
+      this.experimentName = option.experimentName;
+      this.experiment = this.experiments.experiments[option.experimentName];
+      this.binder = new Binder.Binder(this.experiment);
+
       try {
-        uni.setStorageSync(this.experimentName, this.items);
+        const value = uni.getStorageSync(this.experimentName);
+        // console.log("onLoad");
+        // console.log(value);
+
+        if (value) {
+          const that = this;
+          uni.showModal({
+            content: "是否恢复上次输入内容",
+            showCancel: true,
+            cancelText: "否",
+            confirmText: "是",
+            success: function(res) {
+              if (res.confirm) {
+                that.items = value;
+                console.log("用户点击确定");
+              } else if (res.cancel) {
+                console.log("用户点击取消");
+                that.items = that.binder.getLabItems(true);
+              }
+              //第一次不监听
+              that.isWatch = true;
+            }
+          });
+        } else {
+          console.log("unable to get storange");
+          this.items = this.binder.getLabItems(true);
+          //第一次不监听
+          this.isWatch = true;
+        }
       } catch (e) {
         console.error(e);
       }
-    }
-  },
-  onLoad(option) {
-    this.experimentName = option.experimentName;
-    this.experiment = this.experiments.experiments[option.experimentName];
-    this.binder = new Binder.Binder(this.experiment);
-
-    try {
-      const value = uni.getStorageSync(this.experimentName);
-      // console.log("onLoad");
-      // console.log(value);
-
-      if (value) {
-        const that = this;
-        uni.showModal({
-          content: "是否恢复上次输入内容",
-          showCancel: true,
-          cancelText: "否",
-          confirmText: "是",
-          success: function (res) {
-            if (res.confirm) {
-              that.items = value;
-              console.log("用户点击确定");
-            } else if (res.cancel) {
-              console.log("用户点击取消");
-              that.items = that.binder.getLabItems(true);
-            }
-            //第一次不监听
-            that.isWatch = true;
+    },
+    onShow() {},
+    watch: {
+      items: {
+        handler: function() {
+          if (this.isWatch === true) {
+            this.dataStorage();
           }
-        });
-      } else {
-        console.log("unable to get storange");
-        this.items = this.binder.getLabItems(true);
-        //第一次不监听
-        this.isWatch = true;
+        },
+        deep: true
       }
-    } catch (e) {
-      console.error(e);
-    }
-  },
-  onShow() {},
-  watch: {
-    items: {
-      handler: function () {
-        if (this.isWatch === true) {
-          this.dataStorage();
-        }
-      },
-      deep: true
-    }
-  },
-  onUnload() {}
-};
+    },
+    onUnload() {}
+  };
 </script>
 
 <style lang="less" scoped>
-.content {
-  position: relative;
-  font-size: 13px;
-  width: 100%;
-}
-.content .lab-title {
-  height: 80rpx;
-  line-height: 80rpx;
-  text-align: center;
-  font-weight: 500;
-  font-size: 40rpx;
-  margin-top: 10rpx;
-}
-.content-button {
-  display: flex;
+  .content {
+    position: relative;
+    font-size: 13px;
+    width: 100%;
+  }
 
-  justify-content: space-between;
+  .content .lab-title {
+    height: 80rpx;
+    line-height: 80rpx;
+    text-align: center;
+    font-weight: 500;
+    font-size: 40rpx;
+    margin-top: 10rpx;
+  }
 
-  margin-top: 20px;
-}
-.content-button .btn-style {
-  flex-basis: 40%;
-  height: 70rpx;
-  line-height: 70rpx;
-  text-align: center;
-  font-size: 30rpx;
-  color: #ffffff;
-}
+  .content-button {
+    display: flex;
 
-.content-button .btn-reset {
-  background: orange;
-}
-.content-button .btn-compute {
-  background: rgb(142, 201, 55);
-}
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-.lab-warning {
-  width: 100%;
-  text-align-last: center;
-  color: #f74a4a;
-  font-size: 25rpx;
-  margin-top: 20rpx;
-}
-.author {
-  font-size: 25rpx;
-  color: #b5b4b3;
-  text-align: center;
-  margin-top: 10rpx;
-}
-.lab-tips {
-  list-style: disc;
-  font-size: 22rpx;
-  color: #b5b4b3;
-  margin: 30rpx 0 0 20rpx;
-}
-.content .space {
-  height: 300rpx;
-}
+    justify-content: space-between;
+
+    margin-top: 20px;
+  }
+
+  .content-button .btn-style {
+    flex-basis: 40%;
+    height: 70rpx;
+    line-height: 70rpx;
+    text-align: center;
+    font-size: 30rpx;
+    color: #ffffff;
+  }
+
+  .content-button .btn-reset {
+    background: orange;
+  }
+
+  .content-button .btn-compute {
+    background: rgb(142, 201, 55);
+  }
+
+  .text-area {
+    display: flex;
+    justify-content: center;
+  }
+
+  .lab-warning {
+    width: 100%;
+    text-align-last: center;
+    color: #f74a4a;
+    font-size: 25rpx;
+    margin-top: 20rpx;
+  }
+
+  .author {
+    font-size: 25rpx;
+    color: #b5b4b3;
+    text-align: center;
+    margin-top: 10rpx;
+  }
+
+  .lab-tips {
+    list-style: disc;
+    font-size: 22rpx;
+    color: #b5b4b3;
+    margin: 30rpx 0 0 20rpx;
+  }
+
+  .content .space {
+    height: 300rpx;
+  }
 </style>

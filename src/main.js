95e3e47fbe28578099fbbfee751c3820b02f24e4
skyclouds2001 <skyclouds2001@163.com>
2022-05-25 15:16:43 +0800

    perf
    移除 axios adapter自main.js

--- src/main.js
+++ src/main.js
@@ -9,33 +9,3 @@ const app = new Vue({
   ...App,
 });
 app.$mount();
-
-// import axios from "axios";
-
-// axios.defaults.adapter = function(config) {
-//   return new Promise((resolve, reject) => {
-//     console.log("AXIOS Request", config);
-//     var settle = require("axios/lib/core/settle");
-//     var buildURL = require("axios/lib/helpers/buildURL");
-//     uni.request({
-//       method: config.method.toUpperCase(),
-//       url: buildURL(config.url, config.params, config.paramsSerializer),
-//       header: config.headers,
-//       data: config.data,
-//       dataType: config.dataType,
-//       responseType: config.responseType,
-//       sslVerify: config.sslVerify,
-//       complete: function complete(response) {
-//         response = {
-//           data: response.data,
-//           status: response.statusCode,
-//           errMsg: response.errMsg,
-//           header: response.header,
-//           config: config
-//         };
-//         console.log("AXIOS Response", response);
-//         settle(resolve, reject, response);
-//       }
-//     });
-//   });
-// };
